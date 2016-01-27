5cb6b4aa4795a6455730e52b980acd656421c63b
Robert Scharpf <rscharpf@Roberts-MacBook-Pro.local>
2016-01-27 18:24:27 -0500

    Updated the tx_hg19 object
    - Includes a column 'cancer_connection'
    - genome build in seqinfo is 'hg19', not GRCh37

--- data-raw/transcripts.R
+++ data-raw/transcripts.R
@@ -1,7 +1,6 @@
 getTranscripts <- function(){
   library(EnsDb.Hsapiens.v75)
   library(TxDb.Hsapiens.UCSC.hg19.refGene)
-  library(bioMaRt)
   edb <- EnsDb.Hsapiens.v75
   ## Get all transcripts defined in Ensembl (version 75):
   tx <- transcripts(edb, columns=c("tx_id", "gene_id", "gene_name"))
@@ -9,6 +8,17 @@ getTranscripts <- function(){
   seqlevelsStyle(tx) <- "UCSC"
   tx <- keepSeqlevels(tx, paste0("chr", c(1:22, "X", "Y", "M")))
   tx <- sort(tx)
+  si <- seqinfo(tx)
+  genome(si) <- "hg19"
+  seqinfo(tx) <- si
+  cancer.genes <- read.csv(file.path("~/Software/svpackages/svfilters/inst/extdata",
+                                     "master_4_3_2014.csv"),
+                         skip=1, header=TRUE,
+                         stringsAsFactors=FALSE)
+  cancer.genes <- cancer.genes[["Gene.Symbol"]]
+  cancer.genes[!cancer.genes %in% tx$gene_name]
+  cancer.genes[match("MYCL1", cancer.genes)] <- "MYCL"
+  tx$cancer_connection <- tx$gene_name %in% cancer.genes
   ## gene_name has the approved HGNC gene name
   tx_hg19 <- tx
   save(tx_hg19, file="~/Software/svpackages/svdata/data/tx_hg19.rda")
