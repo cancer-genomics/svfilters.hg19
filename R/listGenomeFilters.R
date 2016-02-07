06357b7f3819f8a9619c83c93243007aff409b70
Robert Scharpf <rscharpf@Roberts-MacBook-Pro.local>
2016-02-07 08:17:18 -0500

    Do not include transcripts in reduceGenomeFilters
    Restrict filters to lymphoblasts.  reduceGenomeFilters does not include
    transcripts, but listGenomeFilters does include transcripts.  Currently
    the transcripts are needed in the amplicon analysis.

--- R/listGenomeFilters.R
+++ R/listGenomeFilters.R
@@ -32,25 +32,18 @@ listGenomeFilters <- function(ucsc_build="hg19"){
   data(gaps_hg19, envir=environment())
   gaps <- get("gaps_hg19")
   centromeres <- gaps[gaps$type=="centromere"]
-
   if(FALSE){
     data(coverage_filters_hg19, envir=environment())
     cnv <- reduce(c(coverage_filters[["amplicon"]],
                     coverage_filters[["deletion"]]))
     out <- reduce(coverage_filters[["outlier"]])
   }
-    data(lymphoblast_filters_hg19, envir=environment())
-    lymphoblast_filters <- get("lymphoblast_filters_hg19")
-  
-    data(normalblood_filters_hg19, envir=environment())
-    normalblood_filters <- get("normalblood_filters_hg19")
-    cnv <- reduce(c(lymphoblast_filters[["amplicon"]],
-                   lymphoblast_filters[["deletion"]],
-                    normalblood_filters[["amplicon"]],
-                    normalblood_filters[["deletion"]]))
-    out <- reduce(c(lymphoblast_filters[["outlier"]],
-                    normalblood_filters[["outlier"]]))
-   list(centromeres=centromeres,
+  data(lymphoblast_filters_hg19, envir=environment())
+  lymphoblast_filters <- get("lymphoblast_filters_hg19")
+  cnv <- reduce(c(lymphoblast_filters[["amplicon"]],
+                  lymphoblast_filters[["deletion"]]))
+  out <- reduce(lymphoblast_filters[["outlier"]])
+  list(centromeres=centromeres,
        assembly_gaps=binAssemblyGaps,
        germline_cnv=cnv,
        outliers=out,
@@ -78,6 +71,7 @@ listGenomeFilters <- function(ucsc_build="hg19"){
 #' 
 reduceGenomeFilters <- function(ucsc_build="hg19", seqlev){
   filters <- listGenomeFilters(ucsc_build)
+  filters <- filters[-match("transcripts", names(filters))]
   r <- reduce(unlist(GRangesList(lapply(filters, granges))))
   if(missing(seqlev)) return(r)
   keepSeqlevels(r, seqlev)  
@@ -95,9 +89,10 @@ reduceGenomeFilters <- function(ucsc_build="hg19", seqlev){
 listRearFilters <- function(ucsc_build="hg19"){
   if(ucsc_build != "hg19") stop("Only available for build hg19")
   data(lymphoblast_rear_hg19, envir=environment())
-  data(normalblood_rear_hg19, envir=environment())
-  GRangesList(lymphoblast=lymphoblast_rear_hg19,
-              normalblood=normalblood_rear_hg19)
+  ##data(normalblood_rear_hg19, envir=environment())
+  ##GRangesList(lymphoblast=lymphoblast_rear_hg19,
+  ##normalblood=normalblood_rear_hg19)
+  lymphoblast_rear_hg19
 }
 
 #' Provides a reduced set of germline rearrangement filters derived
