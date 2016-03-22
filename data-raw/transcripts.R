686bceaccff5713c0e445ffcd87e88e90e4c930f
Robert Scharpf <rscharpf@roberts-mbp.mtw.jhu.edu>
2016-03-22 13:04:13 -0400

    Define refseq-derived transcripts
    Replaces transcripts.rda that was previously derived from Ensemble

--- data-raw/transcripts.R
+++ data-raw/transcripts.R
@@ -1,7 +1,31 @@
-library(EnsDb.Hsapiens.v75)
-edb <- EnsDb.Hsapiens.v75
-## Get all transcripts defined in Ensembl (version 75):
-tx <- transcripts(edb, columns=c("tx_id", "gene_id", "gene_name"))
+##library(EnsDb.Hsapiens.v75)
+##library(GenomeInfoDb)
+##edb <- EnsDb.Hsapiens.v75
+#### Get all transcripts defined in Ensembl (version 75):
+##tx <- transcripts(edb, columns=c("tx_id", "gene_id", "gene_name"))
+##seqlevelsStyle(tx) <- "UCSC"
+##tx <- keepSeqlevels(tx, paste0("chr", c(1:22, "X", "Y", "M")))
+##tx <- sort(tx)
+##si <- seqinfo(tx)
+##genome(si) <- "hg19"
+##seqinfo(tx) <- si
+library(TxDb.Hsapiens.UCSC.hg19.refGene)
+library(AnnotationHub)
+tx <- transcripts(TxDb.Hsapiens.UCSC.hg19.refGene)
+ah <- AnnotationHub()
+query(ah, "OrgDb")
+hsapiens <- ah[["AH49582"]]
+keytypes(hsapiens)
+refseqid <- keys(hsapiens, "REFSEQ")
+map <- select(hsapiens, refseqid, "SYMBOL", "REFSEQ")
+table(tx$tx_name %in% map$REFSEQ)
+## 66 of the transcripts are not in the map
+tx$tx_name[! tx$tx_name %in% map$REFSEQ]
+tx <- tx[ tx$tx_name %in% map$REFSEQ ]
+symbols <- setNames(map$SYMBOL, map$REFSEQ)
+symbols <- symbols[tx$tx_name]
+tx$gene_name <- symbols
+
 library(GenomeInfoDb)
 seqlevelsStyle(tx) <- "UCSC"
 tx <- keepSeqlevels(tx, paste0("chr", c(1:22, "X", "Y", "M")))
@@ -9,6 +33,7 @@ tx <- sort(tx)
 si <- seqinfo(tx)
 genome(si) <- "hg19"
 seqinfo(tx) <- si
+
 df <- read.csv("~/Software/svpackages/svfilters/inst/extdata/cancer_genes_2016-03-05.csv",
                stringsAsFactors=FALSE)
 df <- df[!is.na(df$biol_sign), ]
