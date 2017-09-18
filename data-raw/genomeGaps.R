74e9055de45c65496f506ff31d1aa1e07f078171
Daniel Bruhm <danielbruhm@gmail.com>
2017-09-18 14:22:57 -0400

    Move code to create gaps for hg18 to svfilters.hg18

--- data-raw/genomeGaps.R
+++ data-raw/genomeGaps.R
@@ -14,16 +14,4 @@ genomeGaps <- function(){
   gaps <- trim(gaps_hg19)
   save(gaps, file="/Users/dbruhm/Desktop/cancer-genomics/svfilters.hg19/data/gaps.rda")
 }
-library(BSgenome.Hsapiens.UCSC.hg18)
-genome <- "hg18"
-mySession <- browserSession()
-genome(mySession) <- genome
-gaps <- getTable(ucscTableQuery(mySession, track="gap"))
-gaps.gr <- GRanges(gaps$chrom, IRanges(gaps$chromStart,
-                                       gaps$chromEnd),
-                   type=gaps$type)
-gaps_hg18 <- gaps.gr
-## no gaps available in mitochondria
-gaps_hg18 <- keepSeqlevels(gaps_hg18, paste0("chr", c(1:22, "X", "Y")))
-seqinfo(gaps_hg18) <- seqinfo(Hsapiens)[seqlevels(gaps_hg18), ]
-save(gaps_hg18, file="/dcl01/scharpf/data/svpackages/svfilters/data/gaps_hg18.rda")
+
